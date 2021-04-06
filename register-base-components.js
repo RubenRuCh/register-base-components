c4769e40a57b96544a60d446056f64b486627877
Rüger <57111022+RubenRuCh@users.noreply.github.com>
2021-04-06 20:42:20 +0200

    First commit

---
+++ register-base-components.js
@@ -0,0 +1,37 @@
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+const requireComponent = require.context(
+  // The relative path of the components folder
+  './components',
+  // Whether or not to look in subfolders
+  true,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+export const registerBaseComponents = app =>
+  requireComponent.keys().forEach(fileName => {
+    // Get component config
+    const componentConfig = requireComponent(fileName);
+
+    // Get PascalCase name of component
+    const componentName = upperFirst(
+      camelCase(
+        // Gets the file name regardless of folder depth
+        fileName
+          .split('/')
+          .pop()
+          .replace(/\.\w+$/, '')
+      )
+    );
+
+    // Register component globally
+    app.component(
+      componentName,
+      /* Look for the component options on `.default`, which will
+       exist if the component was exported with `export default`,
+       otherwise fall back to module's root */
+      componentConfig.default || componentConfig
+    );
+  });
