5f7ad797834f5175c0b9cf7bbf1d67696cf33e7e
Rüger <57111022+RubenRuCh@users.noreply.github.com>
2021-04-08 14:01:00 +0200

    Load components in lazy way (dynamic components)

--- register-base-components.js
+++ register-base-components.js
@@ -7,31 +7,33 @@ const requireComponent = require.context(
   // Whether or not to look in subfolders
   true,
   // The regular expression used to match base component filenames
-  /Base[A-Z]\w+\.(vue|js)$/
+  /Base[A-Z]\w+\.(vue|js)$/,
+  // How to load the component. In this case, do it dynamically (just when)
+  'lazy'
 );
 
 export const registerBaseComponents = app =>
   requireComponent.keys().forEach(fileName => {
     // Get component config
-    const componentConfig = requireComponent(fileName);
+    requireComponent(fileName).then(componentConfig => {
+      // Get PascalCase name of component
+      const componentName = upperFirst(
+        camelCase(
+          // Gets the file name regardless of folder depth
+          fileName
+            .split('/')
+            .pop()
+            .replace(/\.\w+$/, '')
+        )
+      );
 
-    // Get PascalCase name of component
-    const componentName = upperFirst(
-      camelCase(
-        // Gets the file name regardless of folder depth
-        fileName
-          .split('/')
-          .pop()
-          .replace(/\.\w+$/, '')
-      )
-    );
-
-    // Register component globally
-    app.component(
-      componentName,
-      /* Look for the component options on `.default`, which will
+      // Register component globally
+      app.component(
+        componentName,
+        /* Look for the component options on `.default`, which will
        exist if the component was exported with `export default`,
        otherwise fall back to module's root */
-      componentConfig.default || componentConfig
-    );
+        componentConfig.default || componentConfig
+      );
+    });
   });
